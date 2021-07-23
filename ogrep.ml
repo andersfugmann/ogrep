597fdc398c9f14db4d71421cf7e5778d277fb095
Anders Fugmann <anders@fugmann.net>
2021-07-23 12:50:45 +0200

    Handle gitignore

--- ogrep.ml
+++ ogrep.ml
@@ -36,47 +36,95 @@ let read_lines file =
     Stdio.In_channel.iter_lines ~f:(fun _ -> Int.incr lines)
   )
 
+(* Create a long list of regexps *)
+let parse_gitignore (path_globs, file_globs) dir =
+  Stdio.In_channel.with_file (dir ^ "/.gitignore") ~f:(fun in_channel ->
+    Stdio.In_channel.input_lines in_channel)
+  |> List.fold_left ~init:(path_globs, file_globs) ~f:(fun (path_globs, file_globs) pattern ->
+    match String.strip pattern with
+    | "" -> (path_globs, file_globs)
+    | p when Char.(p.[0] = '#') -> (path_globs, file_globs)
+    | p when Char.(p.[0] = '!') ->
+      eprintf "Argh - Ignore pattern: %s/.gitignore \n%!" dir;
+      (path_globs, file_globs)
+    | p when String.contains p '/' ->
+      (* Standard glob pattern *)
+      let re =
+        dir ^ "/" ^ p
+        |> Re.Glob.glob ~anchored:true
+        |> Re.compile
+      in
+      (re :: path_globs, file_globs)
+    | p ->
+      let re =
+        p
+        |> Re.Glob.glob ~anchored:true
+        |> Re.compile
+      in
+      (path_globs, re :: file_globs)
+  )
 
-(* Return a sequence with all files *)
-(* We need a state. And use Base.Sequence. *)
-let find_all_files ~exclude dir =
-  let files_in_dir acc dir =
+(* Return a sequence with all files. *)
+let find_all_files dir =
+  (* We should not filter here, as we dont yet have the .gitignore file *)
+  let files_in_dir dir =
     let dir_handle = Unix.opendir dir in
-    let rec inner acc dir_handle =
+    let rec inner (gitignore, paths) dir_handle =
       match Unix.readdir dir_handle with
-      | path when exclude path -> inner acc dir_handle
-      | path -> inner ((dir ^ "/" ^ path) :: acc) dir_handle
-      | exception End_of_file -> acc
+      | ".gitignore" -> inner (true, paths) dir_handle
+      | path when Char.(path.[0] = '.') ->
+        (* Quickly ignore hidden files *)
+        inner (gitignore, paths) dir_handle
+      | path -> inner (gitignore, path :: paths) dir_handle
+      | exception End_of_file -> (gitignore, paths)
     in
-    let paths = inner acc dir_handle in
+    let (has_gitignore, paths) = inner (false, []) dir_handle in
     Unix.closedir dir_handle;
-    paths
+    (has_gitignore, paths)
   in
 
   let rec inner = function
-    | [] -> None
-    | path :: paths ->
-       match Unix.stat path with
-       | Unix.{ st_kind = S_DIR; _ } ->
-          let paths = files_in_dir paths path in
-          inner paths
-       | Unix.{ st_kind = S_REG; _ } ->
-          Some (path, paths)
-       | _ -> inner paths
-       | exception _ -> inner paths
+    | ([]) -> None
+    (* How do we pop? Return a new sequence? Yes! *)
+    | ((path, ignore_patterns) :: paths) ->
+      (* We can optimize this out, as its already returned by readdir *)
+      match Unix.stat path with
+      | Unix.{ st_kind = S_DIR; _ } ->
+        let (has_gitignore, files) = files_in_dir path in
+        let ignore_patterns = match has_gitignore with
+          | true ->
+            let p = parse_gitignore ignore_patterns path in
+            p
+          | false -> ignore_patterns
+        in
+        let paths =
+          List.fold_left ~init:paths ~f:(fun acc file ->
+            let (path_globs, file_globs) = ignore_patterns in
+            let filter = List.exists ~f:(fun pattern ->
+              Re.execp pattern file) file_globs
+            in
+            match filter with
+            | true -> acc
+            | false ->
+              let file = path ^ "/" ^ file in
+              let filter = List.exists ~f:(fun pattern ->
+                Re.execp pattern file) path_globs
+              in
+              match filter with
+              | true -> acc
+              | false -> (file, ignore_patterns) :: acc
+          ) files
+        in
+        inner (paths)
+      | Unix.{ st_kind = S_REG; _ } ->
+        Some (path, paths)
+      | _ -> inner paths
+      | exception _ -> inner paths
   in
   Sequence.unfold ~init:[dir] ~f:inner
 
 let _ =
-  let exclude = function
-    | "_build" -> true
-    | "." -> true
-    | ".." -> true
-    | path when Char.(path.[0] = '.') -> true
-    | _ -> false
-  in
-  (* Ah. We cannot stat, because we need to know the path *)
-  let files = find_all_files ~exclude ".." in
+  let files = find_all_files ("..", ([], [])) in
   let file_count = Sequence.fold ~init:0 ~f:(fun acc _ -> 1 + acc) files in
   (* read_lines "ogrep.ml"; *)
   printf "Files: %d\n" file_count;
